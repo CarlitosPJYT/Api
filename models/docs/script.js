3017c3c1e6aca5875510be946dd2df73ac7d0a49
CarlitosJPYT <59419806+CarlitosJPYT@users.noreply.github.com>
2020-07-19 15:05:29 -0500

    lock

--- models/docs/script.js
+++ models/docs/script.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const productRoutes = require('./routes/product');
 
 require('./db')
 
-app.get('/', (req, res) => {res.send('Hello World')})
+app.use(productRoutes)
 app.listen(3000)
+
 console.log('Server on port', 3000)
